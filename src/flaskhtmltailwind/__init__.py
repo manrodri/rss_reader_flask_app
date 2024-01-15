192aac8f755a088e538909c2a72665483775eb3d
EC2 Default User <ec2-user@ip-172-31-5-80.eu-west-1.compute.internal>
2024-01-15 18:35:40 +0000

    Creating our RSS feed lesson: Not workking

--- src/flaskhtmltailwind/__init__.py
+++ src/flaskhtmltailwind/__init__.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template
+import jinja_partials
+import feedparser
 
-app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello World"
+feed = {
+    "title": "The Teclado blog",
+    "href": "https://blog.teclado.com/rss/",
+    "show_images": True,
+    "entries": {
+        
+    }
+}
+
+def create_app():
+    app = Flask(__name__)
+    jinja_partials.register_extensions(app)
+    
+    @app.route('/feed/')
+    def render_feed():
+        feed_url = feed['herf']
+        parsed_feed = feedparser.parse(feed_url)
+        
+        for entry in parsed_feed.entries:
+            if entry.link not in feed['entries']:
+                feed['entries'][entry.link] = entry
+        return render_template('feed.html', feed=feed, entries=feed['entries'].values())
+    
+    return app
