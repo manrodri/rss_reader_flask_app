7681f2b8585abd005d7d309464c40f057ae664bb
Manuel Rodriguez <lolo.edinburgh@gmail.com>
2024-01-16 21:01:12 +0000

    add multiple feeds to __init__.py

--- src/flaskhtmltailwind/__init__.py
+++ src/flaskhtmltailwind/__init__.py
@@ -3,27 +3,40 @@ import jinja_partials
 import feedparser
 
 
-feed = {
+feeds = {
+    "https://blog.teclado.com/rss/":  {
     "title": "The Teclado blog",
     "href": "https://blog.teclado.com/rss/",
     "show_images": True,
-    "entries": {
-        
+    "entries": {   
+    }
+},
+    "https://www.joshwcomeau.com/rss.xml": {
+        "title": "Josh Comeau",
+        "href":  "https://www.joshwcomeau.com/rss.xml",
+        "show_images": False,
+        "entries": {}
     }
 }
+
 
 def create_app():
     app = Flask(__name__)
     jinja_partials.register_extensions(app)
     
-    @app.route('/feed/')
-    def render_feed():
-        feed_url = feed['href']
-        parsed_feed = feedparser.parse(feed_url)
+    @app.route('/')
+    @app.route('/feed/<path:feed_url>')
+    def render_feed(feed_url:str = None):
+        for url, feed_ in feeds.items():
+            parsed_feed = feedparser.parse(url)
+            for entry in parsed_feed.entries:
+                if entry.link not in feed_['entries']:
+                    feed_['entries'][entry.link] = entry
         
-        for entry in parsed_feed.entries:
-            if entry.link not in feed['entries']:
-                feed['entries'][entry.link] = entry
-        return render_template('feed.html', feed=feed, entries=feed['entries'].values())
+        if feed_url is None:
+            feed = list(feeds.values())[0]
+        else:
+            feed = feeds[feed_url]
+        return render_template('feed.html', feed=feed, entries=feed['entries'].values(), feeds=feeds)
     
     return app
